188e952ec383c6317be0e06de6a850e9265de7b5
Dipty Kumari <91672379+DIPTY23@users.noreply.github.com>
2025-11-13 20:45:11 +0530

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,6 @@
+// Static unit from original Game class, can be moved to a Constants class or GameWindow
+package Dino_Game_java;
+
+class Game { // Keep this for unit, or move Game.unit to a Constants file.
+    static int unit = 10;
+}
